82b087725351c68c243b15a5a2e550f44f842ce5
guymeron <guy.meron@gmail.com>
2025-12-14 14:46:20 +0200

    fix: add latency to log

--- periodic/periodic.go
+++ periodic/periodic.go
@@ -829,12 +829,12 @@ MainLoop:
 
 		if metadata != nil {
 			upstreamSvcTime := metadata["X-Envoy-Upstream-Service-Time"]
-			log.Warnf("setupstreamSvcTime: %v", upstreamSvcTime)
+			//log.Warnf("setupstreamSvcTime: %v", upstreamSvcTime)
 			if upstreamSvcTime != nil {
 				duration, err := time.ParseDuration(fmt.Sprintf("%sms", upstreamSvcTime[0]))
 				if err == nil {
 					latency = (e2eDuration - duration).Seconds()
-					log.Warnf("latency: %v", upstreamSvcTime)
+					//log.Warnf("latency: %v", upstreamSvcTime)
 				}
 			}
 		}
